49a51f86083f22fb39c7dc1409b2845671a6d8b3
gioele-bernardini <gioele.g@outlook.com>
2024-03-25 20:25:17 +0100

    .

--- C/build-up/factorial.c
+++ C/build-up/factorial.c
@@ -9,9 +9,22 @@ int factorial(int n) {
   return n * factorial(n - 1);
 }
 
+int factorial2(int n) {
+  if (n == 1 || n == 0) {
+    return 1;
+  }
+
+  long long int out = n;
+  while (--n) {
+    out *= n;
+  }
+
+  return out;
+}
+
 int main(int argc, char** argv) {
   if (argc != 2);
 
-  printf("%d\n", factorial(atoi(argv[1])));
+  printf("%d\n", factorial2(atoi(argv[1])));
   return 0;
 }

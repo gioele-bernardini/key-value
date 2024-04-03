9b019afc98abe43c9b8878c8bbe2dc72ecde29a3
gioele-bernardini <gioele.g@outlook.com>
2024-04-04 00:45:11 +0200

    .

---
+++ leetcode/6_zig.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Solution:
+  def convert(self, s: str, numRows: int) -> str:
+    if numRows == 1: return s
+
+    res = ""
+    for row in range(numRows):
+      increment = 2 * (numRows - 1)
+      for i in range(row, len(s), increment):
+        res += s[i]
+
+        if (row > 0 and row < numRows - 1 and
+            i + increment - 2 * row < len(s)):
+          res += s[i + increment - 2 * row]
+
+    return res

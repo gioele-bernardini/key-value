34518e357373805d9178351dfdf248031041f54c
gioele-bernardini <gioele.g@outlook.com>
2024-04-16 22:39:56 +0200

    .

--- leetcode/9_palindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-  def isPalindrome(x: int) -> bool:
-    return str(x) == str(x)[::-1]
-
-if __name__ == '__main__':
-  solution = Solution
-  out = solution.isPalindrome(13)
-  print(out)
--- temp.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  ciaone("ciao, questa e' la prima riga!");
-  ciaone("e infine questa e' la seconda :)");
-  ciaone un commento => ciao!
-}

ea4abcdb443b9fb49a3694a5de4cfc31182c98e0
gioele-bernardini <gioele.g@outlook.com>
2024-04-21 20:43:23 +0200

    .

--- rust/weather_app/target/debug/deps/weather_app-e27f57c421d4efc4.d
+++
@@ -1,5 +0,0 @@
-/home/gioele/low-stuff/rust/weather_app/target/debug/deps/libweather_app-e27f57c421d4efc4.rmeta: src/main.rs
-
-/home/gioele/low-stuff/rust/weather_app/target/debug/deps/weather_app-e27f57c421d4efc4.d: src/main.rs
-
-src/main.rs:

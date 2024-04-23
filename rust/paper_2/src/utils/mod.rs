48caac1448f0897c3bb055e591e64a214efb2261
gioele-bernardini <gioele.g@outlook.com>
2024-04-23 23:59:19 +0200

    .

--- rust/paper_2/src/utils/mod.rs
+++ rust/paper_2/src/utils/mod.rs
@@ -1,4 +1,5 @@
 use core::slice;
+use std::slice::Iter;
 
 use rand::prelude::*;
 
@@ -143,3 +144,48 @@ pub fn insert_if_longer(v: &mut Vec<String>, s: &String) {
   }
 }
 
+pub fn build_vector(iter: Iter<i32>) -> Vec<&i32> {
+  let mut out: Vec<&i32> = Vec::new();
+  // let vector = iter.collect();
+
+  for i in iter {
+    out.push(i);
+  }
+
+  out
+}
+
+fn flip(vector: &mut Vec<i32>, k: usize) {
+  let (left, _) = vector.split_at_mut(k +1);
+  
+  left.reverse();
+}
+
+fn find_max(vector: &[i32]) -> usize {
+  let mut index = 0;
+
+  for i in 0..vector.len() {
+    if vector[i] > vector[index] {
+      index = i;
+    }
+  }
+
+  index
+}
+
+pub fn pancake_sort(vector: &mut Vec<i32>) {
+  let mut index = vector.len();
+
+  while index > 0 {
+    let (first_half, _) = vector.split_at(index);
+    index -= 1;
+    let max_index = find_max(first_half);
+
+    if index != max_index {
+      flip(vector, max_index);
+      flip(vector, index);
+    }
+  }
+}
+
+// TODO: Pancake Sort once more!

82ccffdb0e61a1b61ead75cc7daa646778393092
gioele-bernardini <gioele.g@outlook.com>
2024-04-24 20:45:53 +0200

    .

--- rust/build_up/src/geeks/mod.rs
+++ rust/build_up/src/geeks/mod.rs
@@ -1,4 +1,5 @@
-use std::env::{self, args_os};
+use std::io::{self, Write};
+use std::env::{ self };
 
 fn greet() {
   let args: Vec<String> = env::args().collect();
@@ -12,3 +13,30 @@ fn greet() {
   let name = &args[1];
   println!("Hello, {}", name);
 }
+
+fn sum() {
+  let mut A = 0;
+  let mut B = 0;
+
+  loop {
+    print!("Please insert two valid numbers => ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let mut parts = input.trim().split_whitespace();
+
+    if let (Some(a_str), Some(b_str)) = (parts.next(), parts.next()) {
+      if let (Ok(A_num), Ok(B_num)) = (a_str.parse::<i32>(), b_str.parse::<i32>()) {
+        A = A_num;
+        B = B_num;
+        break;
+      }
+    }
+  }
+
+  let sum = A + B;
+  println!("The result is => {}", sum);
+}
+

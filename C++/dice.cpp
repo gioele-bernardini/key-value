cc50f408b13154e0962054119397b70f17513fb0
gioele-bernardini <gioele.g@outlook.com>
2024-05-01 11:27:19 +0200

    .

--- C++/dice.cpp
+++ C++/dice.cpp
@@ -1,11 +1,28 @@
-#include <iostream>
+#include <iostream> // Drops .h (still available tho)
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-const int sides = 6;
+const int sides = 6;  // #define SIDES 6
 
+// Direttiva compiler
+// I have a function, I don't want a function call!
 inline int r_sides() {
   return (rand() % sides +1);
+}
+
+int main(void) {
+  const int n_dice = 2;
+  int d1, d2;
+
+  srand(clock());
+  cout << "\nEnter number of trials : ";
+  int trials;
+
+  // Type safety implicita!
+  cin >> trials;  // Compared to scanf()
+
+  // Per rappresentare i numeri da 0 a 12 (13 valori!)
+  int* outcomes = new int[n_dice * sides +1];
 }

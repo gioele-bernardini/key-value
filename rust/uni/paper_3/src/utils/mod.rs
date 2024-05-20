714991a5d2bee7e988157416b2f042f186c8964e
gioele-bernardini <gioele.g@outlook.com>
2024-05-20 11:55:11 +0200

    more exercises completed

--- rust/uni/paper_3/src/utils/mod.rs
+++ rust/uni/paper_3/src/utils/mod.rs
@@ -112,10 +112,76 @@ impl VendingMachine {
   }
 
   fn add_item(&mut self, item: Item, quantity: usize) {
-    
+    self.items.insert(item, quantity);
+  }
+
+  fn insert_coin(&mut self, coin: Coin) {
+    match coin {
+      Coin::Cent10 => self.coins += 10,
+      Coin::Cent20 => self.coins += 20,
+      Coin::Cent50 => self.coins += 50,
+      Coin::Eur1 => self.coins += 100,
+      Coin::Eur2 => self.coins += 200,
+    }
+  }
+
+  fn get_item_price(&self, item: &Item) -> u8 {
+    match item {
+      Item::Water => 150,
+      Item::Coca => 200,
+      Item::Soda => 200,
+      Item::Tea => 180,
+    }
+  }
+
+fn buy(&mut self, item: &Item) -> Result<(), String> {
+    let price = self.get_item_price(item) as u32;
+
+    if self.coins < price {
+        return Err("Not enough money".to_string());
+    }
+
+    if let Some(quantity) = self.items.get_mut(&item) {
+        if *quantity > 0 {
+            let change = self.coins - price;
+            *quantity -= 1;
+            return Ok(());
+        }
+    }
+
+    Err("Item not available".to_string())
+}
+
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Date(u8, u8, u16);
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Hour(u8, u8);
+
+pub struct BoxShipping {
+  name: String,
+  barcode: String,
+  shipment_date: Date,
+  shipment_hour: Hour,
+}
+
+use std::fmt::{ Debug, Display, Formatter };
+
+impl Display for BoxShipping {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{} {} {:?} {:?}", self.name, self.barcode, self.shipment_date, self.shipment_hour)
   }
 }
 
+impl Debug for BoxShipping {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "Debug: {} {} {:?} {:?}", self.name, self.barcode, self.shipment_date, self.shipment_hour)
+  }
+}
 
+// TODO: implement the other methods
 
+// TODO: library system
 

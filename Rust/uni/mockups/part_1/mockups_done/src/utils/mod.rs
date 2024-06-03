be1a77cf1220f13c9e927a3b8cf72ec39767146f
gioele-bernardini <gioele.g@outlook.com>
2024-06-03 23:22:28 +0200

    mockup 1 started

---
+++ Rust/uni/mockups/part_1/mockups_done/src/utils/mod.rs
@@ -0,0 +1,13 @@
+// Write a function prev_str that takes a &str as input and returns a String.
+
+// This function converts the &str by replacing each character with its predecessor. For example:
+
+// 'b' becomes 'a'
+// 'f' becomes 'e'
+// 'B' becomes 'A'
+// 'A' remains 'A'
+// 'a' remains 'a'
+// If the character is not a letter, it remains unchanged.
+
+
+

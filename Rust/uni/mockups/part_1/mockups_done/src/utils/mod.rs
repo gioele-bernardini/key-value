ede9a80257982165b945fb3eaac721203200ec07
gioele-bernardini <gioele.g@outlook.com>
2024-06-10 17:16:29 +0200

    rust: few functions added.

---
+++ Rust/uni/mockups/part_1/mockups_done/src/utils/mod.rs
@@ -0,0 +1,54 @@
+pub fn prev_str(input: &str) -> String {
+  input.chars().map(|c|
+    if c.is_ascii_alphabetic() {
+      match c {
+        'a' | 'A' => c,
+        'b' ..= 'z' => (c as u8 -1) as char,
+        'B' ..= 'Z' => (c as u8 -1) as char,
+        _ => c,
+      }
+    } else {
+      c
+    }
+  ).collect()
+}
+
+pub fn prev_str2(input: &str) -> String {
+  let mut out = String::new();
+
+  for c in input.chars() {
+    match c {
+      'a' | 'A' => out.push(c),
+      'b' ..= 'z' => out.push((c as u8 -1) as char),
+      'B' ..= 'Z' => out.push((c as u8 -1) as char),
+      _ => out.push(c)
+    }
+  }
+
+  out
+}
+
+pub struct X {
+  s: Option<String>,
+  i: i32
+}
+
+impl X {
+  fn new(s: &str, i: i32) -> Self {
+    Self {
+      s: Some(s.to_string()),
+      i,
+    }
+  }
+
+  fn take_str(&mut self) -> Option<String> {
+    self.s.take()
+  }
+
+  fn take_str2(&mut self) -> Option<String> {
+    let out = self.s.clone();
+    self.s = None;
+
+    out    
+  }
+}

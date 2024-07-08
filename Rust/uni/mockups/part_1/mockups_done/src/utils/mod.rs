b5c0494c7d95110812d84dc3802240ab501046e0
gioele-bernardini <gioele.g@outlook.com>
2024-07-08 15:39:17 +0200

    .

--- Rust/uni/mockups/part_1/mockups_done/src/utils/mod.rs
+++ Rust/uni/mockups/part_1/mockups_done/src/utils/mod.rs
@@ -1,3 +1,5 @@
+use std::mem::swap;
+
 fn prev_str(s: &str) -> String {
   s.chars().map(|c|
   match c {
@@ -24,4 +26,33 @@ impl X {
   fn take_str(&mut self) -> Option<String> {
     self.s.take()
   }
+}
+
+struct NameSurname {
+  name: String,
+  surname: String,
+}
+
+fn replace_surname(ns: &mut NameSurname, s: String) -> String {
+  // ns.surname e basta non va, avviene un move!
+  let out = ns.surname.clone();
+  ns.surname = s;
+
+  out
+}
+
+fn replace_surname2(ns: &mut NameSurname, s: String) -> String {
+  let mut old_surname = s;
+  swap(&mut ns.surname, &mut old_surname);
+
+  old_surname
+}
+
+struct Student {
+  name: String,
+  id: u32,
+}
+
+impl Student {
+
 }

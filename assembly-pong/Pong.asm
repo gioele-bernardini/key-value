127fc9ac1696e06ed49fc97defd876d7d3f456d0
Gioele Bernardini <gioele.g@outlook.com>
2024-07-20 21:22:27 +0200

    Add: configuration to drawing an initial simple pixel completed.

--- assembly-pong/Pong.asm
+++ assembly-pong/Pong.asm
@@ -11,9 +11,21 @@ CODE SEGMENT PARA 'CODE'
 
   MAIN PROC FAR
   
-    MOV DL, 'A'
-    MOV AH, 6h
-    INT 21h
+    MOV AH, 00h ; Set the configuration to video mode
+    MOV AL, 13h ; Choose the video mode
+    INT 10h ; Execute the configuration
+
+    MOV AH, 0Bh ; Set the configuration
+    MOV BH, 00h ; To the background color
+    MOV BL, 00h ; Choose black as background color
+    INT 10h ; Execute the configuration
+
+    MOV AH, 0Ch ; Set the configuration to writing a pixel
+    MOV AL, 0Fh ; Choose white as color
+    MOV BH, 00h ; Set the page number
+    MOV CX, 0Ah ; Set the column (X position)
+    MOV DX, 0Ah ; Set the line (Y position)
+    INT 10h
 
     RET
   MAIN ENDP
